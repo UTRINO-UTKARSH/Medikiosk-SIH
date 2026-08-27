e6398d9af095fd59bbc339b760bf2093f3015404
Utrino <utkarshbhaskar10@gmail.com>
2026-08-27 11:54:20 +0530

    new

--- Frontend/src/components/hero/Middle.jsx
+++ Frontend/src/components/hero/Middle.jsx
@@ -11,13 +11,13 @@ const Middle = () => {
       <Content />
       <HeroImage />
       <div className="flex">
-        <div className="h-18 w-60 bg-blue-950 rounded-[8px] justify-items-center content-center mt-6 mr-2">
+        <div className="h-18 w-60 bg-blue-950 rounded-lg justify-items-center content-center mt-6 mr-2">
           <div className="flex justify-between w-20 font-bold text-white font-robot text-xl items-center">
             Start
             <MoveRight />
           </div>
         </div>
-        <div className="h-18 w-60 bg-gray-300 rounded-[8px] border-0.5 justify-items-center content-center mt-6 ml-2">
+        <div className="h-18 w-60 bg-gray-300 rounded-lg border-0.5 justify-items-center content-center mt-6 ml-2">
           <div className="flex justify-between w-20 font-bold text-black font-robot text-xl items-center">
             <CircleQuestionMark />
             Help
