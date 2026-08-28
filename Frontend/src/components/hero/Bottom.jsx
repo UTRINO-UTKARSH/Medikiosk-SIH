cc46f170dff9cc1211f1a32322612b74a9b06286
Anandagarwall <anandagarwal260@gmail.com>
2026-08-28 11:37:55 +0530

    updated Home page

--- Frontend/src/components/hero/Bottom.jsx
+++ Frontend/src/components/hero/Bottom.jsx
@@ -4,15 +4,15 @@ import Buttons from '../common/Buttons'
 
 const Bottom = () => {
   return (
-    <div className='h-[18%] bottom-0 overflow-hidden  w-full fixed z-100 flex-col justify-items-center '>
-        <div className='h-0.5 w-[60%] bg-gray-300 mb-3 top-0'></div>
-        <p className='font-sans font-medium text-gray-600 text-2xl'>Choose your language</p>
-        <div className='Container w-full flex justify-center'>
-            <Buttons text={"English"} design={"px-10 py-4 cursor-pointer border-2 rounded-[5px] font-medium text-[24px] text-blue-950 border-gray-400 m-6 bg-white"} />
+    <div className='h-1/5 overflow-hidden w-full flex-col justify-items-center '>
+        <div className='h-0.5 w-[80%] md:w-[60%] bg-gray-300 mb-3 top-0'></div>
+        <p className='font-sans font-medium text-gray-600 text-xl md:text-2xl'>Choose your language</p>
+        <div className='Container w-full flex justify-center mt-5 md:mt-2'>
+            <Buttons text={"English"} design={"px-2 md:px-6 py-2 cursor-pointer border-2 rounded-[5px] font-medium text-[20px] md:text-[24px] text-blue-950 border-gray-400 m-2 md:m-4 bg-white"} />
             
-            <Buttons text={"Hindi"} design={"px-10 py-4 cursor-pointer border-2 rounded-[5px] font-medium text-[24px] text-blue-950 border-gray-400 m-6 bg-white"} />
+            <Buttons text={" हिन्दी(Hindi)"} design={"px-2 md:px-6 py-2 cursor-pointer border-2 rounded-[5px] font-medium text-[20px] md:text-[24px] text-blue-950 border-gray-400 m-2 md:m-4 bg-white"} />
 
-            <Buttons text={"Marathi"} design={"px-10 py-4 cursor-pointer border-2 rounded-[5px] font-medium text-[24px] text-blue-950 border-gray-400 m-6 bg-white"} />
+            <Buttons text={"मराठी(Marathi)"} design={"px-2 md:px-6 py-2 cursor-pointer border-2 rounded-[5px] font-medium text-[20px] md:text-[24px] text-blue-950 border-gray-400 m-2 md:m-4 bg-white"} />
         </div>
     </div>
   )
--- Frontend/src/components/hero/HeroImage.jsx
+++ Frontend/src/components/hero/HeroImage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const HeroImage = () => {
   return (
-    <div className='justify-items-center content-center'>
-        <img className='h-auto w-110 rounded-2xl border-2 border-gray-300'
+    <div className='justify-items-center content-center mb-5 md:m-2'>
+        <img className='h-auto w-90 md:w-120 rounded-2xl border-2 border-gray-300'
         src="https://lh3.googleusercontent.com/aida-public/AB6AXuDQeCPVKmqFfnqSPXzxBF5QRBi9feBp840ZjUvKYpLnoYGmPcm5OSa6NeDbUx20VD8E-SmgseBlhTA4Zpj9nceuIMz26e4rDjAIPPG9RHLxlQyVuTH7AgGHLG0RNqE9cFqIjTZbhKrX8rE9jBZyJVWUb3OG1sxbOfsnhuoB4rkz3l4ILLqopMQF459fb3Veu_YVp-N0DG4M8sNhzqCdwmcPtO6D8iWXLL22XFMYyUiHaNECbXOyTlup" alt=""/>
     </div>
   )
